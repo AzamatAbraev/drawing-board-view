94145784b403f9573917bde2299c2ab27a367361
Your Name <azamatabraev07@gmail.com>
2024-02-22 13:58:52 +0500

    socket url changed

--- src/server/socket.ts
+++ src/server/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "https://drawing-board.up.railway.app/api/";
+const URL = "https://drawing-board.up.railway.app/";
 const socket = io(URL);
 
 export default socket;
